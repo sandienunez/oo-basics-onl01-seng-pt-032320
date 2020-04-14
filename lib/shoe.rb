ad44674416aca913d5fa1ca45f43a62dd8bc0b8d
Sandie Nunez <snunez@vt.edu>
2020-04-14 01:27:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :type
+  attr_reader :designer
+ 
+  def initialize(designer)
+    @designer = designer
+  end
+ 
+  def new_shoe
+    puts "Thank you for purchasing a shoe for the poor!"
+  end
+ 
+end
