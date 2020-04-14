344d4598b8be8b33543b806419a643203b11f4ab
Sandie Nunez <snunez@vt.edu>
2020-04-14 01:23:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,4 @@
-require "pry"
+
 class Book 
   def initialize(title)
     @title = title 
@@ -34,6 +34,6 @@ class Book
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
-    binding.pry
+
   end
 end 
